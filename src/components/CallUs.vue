be068023baace20352b22c4f5ed2bf47ee27bc08
cosmasken <cosmasarubaken@gmail.com>
2024-08-29 17:02:05 +0300

    prod link works

--- src/components/CallUs.vue
+++ src/components/CallUs.vue
@@ -1,7 +1,7 @@
 <template>
        <!-- Call Button -->
       <p
-        class="flex items-center  text-black font-semibold text-lg"
+        class="flex items-center  text-red-500 font-semibold text-lg"
       >
         <!-- Button Text -->
         Call us on 0720769045
